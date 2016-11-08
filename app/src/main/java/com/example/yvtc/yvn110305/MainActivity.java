6bbdc8fda605bf639d40ef0835afdaff75826045
shaojung <shaojung@gmail.com>
2016-11-08 19:40:25 +0800

    add checkbox in custom view

--- app/src/main/java/com/example/yvtc/yvn110305/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn110305/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -14,7 +16,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
-    String[] fruits = {"蘋果", "香蕉", "鳳梨", "芭樂", "檸檬"};
+    String[] fruits = {"蘋果", "香蕉", "鳳梨", "芭樂", "檸檬", "蘋果1", "香蕉1", "鳳梨1", "芭樂1", "檸檬1"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,4 +27,15 @@ public class MainActivity extends AppCompatActivity {
         lv.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Show");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
 }
